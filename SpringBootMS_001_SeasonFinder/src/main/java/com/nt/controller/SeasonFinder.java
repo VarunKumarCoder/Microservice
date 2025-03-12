729eaea2a38a39e5e867347729f48cc319a5ae2d
VarunKumarCoder <medavarunkumar@gmail.com>
2025-03-12 18:02:22 +0530

    Season Finder Client app

---
+++ SpringBootMS_001_SeasonFinder/src/main/java/com/nt/controller/SeasonFinder.java
@@ -0,0 +1,10 @@
+package com.nt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/season")
+public class SeasonFinder {
+
+}
