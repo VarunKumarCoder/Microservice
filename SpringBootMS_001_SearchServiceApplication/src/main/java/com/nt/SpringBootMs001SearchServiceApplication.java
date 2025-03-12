f4e133fc01c25ee7f00d4e9ebf2b337ca6dc1311
VarunKumarCoder <medavarunkumar@gmail.com>
2025-03-12 17:10:31 +0530

    Search Service Application as Eureka Client

---
+++ SpringBootMS_001_SearchServiceApplication/src/main/java/com/nt/SpringBootMs001SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMs001SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMs001SearchServiceApplication.class, args);
+	}
+
+}
